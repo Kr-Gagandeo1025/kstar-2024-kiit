71de74ec6a8788e143907c438ae0a1ded641255c
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2024-12-18 12:22:38 +0530

    ui: prize change

--- app/components/sections/HeroSection.jsx
+++ app/components/sections/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <NavBar page={"home"}/>
       <div className="flex lg:flex-row flex-col items-center w-full justify-between lg:px-32 px-5 lg:mt-16 mt-10">
         <img src="\images\header-event-img.svg" alt="hero-main-kstar" className='lg:h-[350px] w-auto object-cover'/>
-        <img src="\images\prize_pool.png" alt="hero-prize-highlight" className='lg:h-[400px] w-auto object-cover'/>
+        <img src="\images\prizes.svg" alt="hero-prize-highlight" className='lg:h-[350px] w-auto object-cover'/>
       </div>
       <div className='mb-[200px] mt-12'>
         <img src="\images\event-date.svg" alt="25th - 27th Dec 2024" className='lg:w-[500px] w-[300px] h-auto'/>
